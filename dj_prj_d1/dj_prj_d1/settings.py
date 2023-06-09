89069995159b84e7ca48cf74d21618be90df07cb
aer <a.ermolaev.ee@gmail.com>
2023-06-09 21:20:39 +0200

    D1.4 fpages admin extended with comments

--- dj_prj_d1/dj_prj_d1/settings.py
+++ dj_prj_d1/dj_prj_d1/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'django.contrib.flatpages'
+    'django.contrib.flatpages',
+    'fpages',
 ]
 
 SITE_ID = 1
